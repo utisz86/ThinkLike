15b6c4b2a3de584447da136e8d000810e9069ff9
utisz86 <soma.kormanik@outlook.com>
2020-06-29 02:13:25 +0300

    7.9ig

---
+++ basic_test.py
@@ -0,0 +1,19 @@
+import sys
+
+def teszt(sikeres_teszt):
+    """ Egy test eredmenyenek megjelenitese.    """
+    sorszam = sys._getframe(1).f_lineno
+    if sikeres_teszt:
+        msg = "A(z) {0}. sorban allo teszt sikeres.".format(sorszam)
+    else:
+        msg = ("A(z) {0}. sorban allo teszt SIKERTELEN".format(sorszam))
+
+    print(msg)
+
+def tesztkeszlet():
+    """ Az ehhez a modulhoz (fajlhoz) tartozo tesztkeszlet futtatasa. """
+    teszt(abs(-5) == 5)
+    #
+
+
+tesztkeszlet()
